e95890146b0b364e676fce46cc96b320dc75902b
America Fernanda Ramirez Rodriguez <fernmirz20@gmail.com>
2024-09-13 20:58:40 -0600

    Antepenultimo commit

---
+++ src/temas-ts/distancia-entre-dos puntos.ts
@@ -0,0 +1,14 @@
+
+function calcularDistancia(punto1: { x: number, y: number }, punto2: { x: number, y: number }): number {
+    const deltaX = punto2.x - punto1.x;
+    const deltaY = punto2.y - punto1.y;
+    return Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+}
+
+
+const puntoA = { x: 1, y: 2 };
+const puntoB = { x: 4, y: 6 };
+
+
+const distancia = calcularDistancia(puntoA, puntoB);
+console.log(`La distancia entre los puntos es ${distancia}`);
